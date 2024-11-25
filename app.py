c50e67dc656d4e74d77f62c9d5c262c299f01874
Parisotto <parisotto@gmail.com>
2024-11-25 10:18:05 -0300

    Atualizações 25/11/2024

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__, 
             template_folder='siteciencia/templates',
@@ -14,6 +15,12 @@ def index():
 def analise(usuario):
   return render_template('analise.html', usuario=usuario)
   
+@app.route("/galeria")
+def galeria():
+  numeros = random.sample(range(1, 26), 15)
+  lista_imagens = [f"{num}.jpg" for num in numeros]
+  return render_template('galeria.html', imagens=lista_imagens)
+
 
 if __name__ == "__main__":
   app.run(debug=True)
